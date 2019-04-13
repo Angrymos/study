c5adfb157f4c4fd997778c39494a84cf66aaf649
Anna Rozhdestvenskaya <angrymos@daotech.ru>
2019-04-13 19:32:10 +0400

    Fp exercise completed

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,15 +1,35 @@
+require 'csv'
+
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(films)
+        count = 0
+        ratings = films.map do |film| 
+          countries = film["country"]&.split(pattern=",")
+          countryCount = countries.nil? ? 0 : countries.length
+          rating = film["rating_kinopoisk"].to_f
+          
+          if (rating.positive? and countryCount >= 2) 
+            count += 1
+            rating
+          else
+            0
+          end
+        end
+       
+        raitingSummary = ratings.reduce(:+)
+        raitingSummary / count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        names = films.map do |film|
+          rating = film["rating_kinopoisk"].to_f
+          rating >= threshold ? film["name"] : ""
+        end
+        
+        count = names.reduce(0) { |sum, name| sum + name.count("и") }
+        puts(count)
       end
     end
   end
